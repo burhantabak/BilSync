fa1bbabd640ea1d910dff3eccc26b4efa79c81eb
burhantabak <burhantabak@gmail.com>
2023-10-25 11:54:18 +0300

    spring init

---
+++ server/src/main/java/tr/edu/bilkent/bilsync/BilSyncApplication.java
@@ -0,0 +1,13 @@
+package tr.edu.bilkent.bilsync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BilSyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BilSyncApplication.class, args);
+	}
+
+}
