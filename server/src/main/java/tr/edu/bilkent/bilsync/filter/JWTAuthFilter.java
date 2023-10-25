fcb693fa518bbf04f31a94f9c6098ddd09af0bae
Tuna <tuna.saygin@ug.bilkent.edu.tr>
2023-10-25 19:55:13 +0300

    authfilter added

---
+++ server/src/main/java/tr/edu/bilkent/bilsync/filter/JWTAuthFilter.java
@@ -0,0 +1,16 @@
+package tr.edu.bilkent.bilsync.filter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+public class JWTAuthFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+    }
+}
