425d0376a92a98410bbe827bf2980785c65f04b0
Tuna <tuna.saygin@ug.bilkent.edu.tr>
2023-12-11 18:22:49 +0300

    createPost Completed

--- Frontend/SynchFront/src/Components/CreatePostPage.jsx
+++ Frontend/SynchFront/src/Components/CreatePostPage.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import CategoryDisclosure from '../statics/CategoryDisclosure'
+import SecondHandForum from '../Forums/SecondHandForum'
 
 export default function CreatePost() {
   const categories = {"Trading":["Second Hand","Borrowing","Donation"],"Lost&Found":[],"Forum Post":["Basic Forum Post"]}
@@ -13,14 +14,14 @@ export default function CreatePost() {
             {Object.keys(categories).map((key,index)=>{
               console.log("key:"+key+"/value: "+categories[key])
               if(categories[key].length){return <CategoryDisclosure key={index} title={key} categoryList={categories[key]}/>}
-              else{return <button className='text-lg font-bold pl-4 pr-2 py-2 w-full hover:bg-gray-200 flex rounded-lg mt-1'>
+              else{return <button key={index} className='text-lg font-bold pl-4 pr-2 py-2 w-full hover:bg-gray-200 flex rounded-lg mt-1'>
                 {key}
               </button>}
             })}
           </div>
         </div>
         <div className='w-2/3'>
-
+            <SecondHandForum/>
         </div>
     </div>
   )
---
+++ Frontend/SynchFront/src/Forums/ForumComponents/HashtagInput.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from 'react'
+
+export const HashtagInput = (props) => {
+  
+    const removeTags = (indexToRemove) => {
+      props.setTags([...props.tags.filter((_, index) => index !== indexToRemove)]);
+    };
+  
+    const addTags = (event) => {
+      if (event.target.value !== "") {
+        props.setTags([...props.tags, event.target.value]);
+        event.target.value = "";
+      }
+    };
+  
+    return (
+      <div className="mx-2">
+        <div className='flex'>
+            <label htmlFor="" className='w-28 font-semibold'> Hashtags: </label>
+            <input
+            name='hashtag'
+            type="text"
+            onKeyUp={(event) => (event.key === "Enter" ? addTags(event) : null)}
+            placeholder="Press enter to add tags"
+            className="bg-gray-50 border border-gray-300
+            text-gray-900 sm:text-sm rounded-lg focus:outline focus:outline-2
+            focus:border-sky-500 block w-full p-2.5 px-3"
+            />
+        </div>
+        <ul id="tags" className="flex flex-wrap pl-28 pr-4">
+          {props.tags.map((tag, index) => (
+            <li
+              key={index}
+              className="tag bg-blue-500 text-white h-8 flex items-center justify-center rounded-md m-2 p-2"
+            >
+              <span className="tag-title">#{tag}</span>
+              <span
+                className="tag-close-icon ml-2 cursor-pointer"
+                onClick={() => removeTags(index)}
+              >
+                x
+              </span>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  };
