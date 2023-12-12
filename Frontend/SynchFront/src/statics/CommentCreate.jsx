41a2499cd14083800349a468dba9ca09fea22893
Kenan <kanan.zeynalov@ug.bilkent.edu.tr>
2023-12-12 18:35:19 +0300

    Post Report pop up page is ready for reporting

--- Frontend/SynchFront/src/statics/CommentCreate.jsx
+++ Frontend/SynchFront/src/statics/CommentCreate.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import { useState } from 'react';
 export default function CommentCreate({isUpvote, setUpvote,isDownvote , setDownvote, vote}) {
   return (
         <div className="flex justify-around w-full items-center pr-1 pl-1">
@@ -32,20 +32,52 @@ export default function CommentCreate({isUpvote, setUpvote,isDownvote , setDownv
   )
 }
 function PostInteraction({isStarred,setStarred}){
+
+  const [showReportBox, setShowReportBox] = useState(false);
+  const [reportReason, setReportReason] = useState('');
+
+  const handleReportClick = () => {
+    setShowReportBox(true);
+    console.log("Report clicked")
+  };
+
+  const handleReportSubmit = () => {
+    console.log('Report submitted with reason:', reportReason);
+
+    // Reset state after submission
+    setShowReportBox(false);
+    setReportReason('');
+  };
+
     return(
-    <div className='pl-1 flex items-center'>
-        <button className='flex items-center flex-col hover:underline' onClick={()=>{setStarred(!isStarred)}}>
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-        <path strokeLinecap="round" strokeLinejoin="round" d="M3 3v1.5M3 21v-6m0 0l2.77-.693a9 9 0 016.208.682l.108.054a9 9 0 006.086.71l3.114-.732a48.524 48.524 0 01-.005-10.499l-3.11.732a9 9 0 01-6.085-.711l-.108-.054a9 9 0 00-6.208-.682L3 4.5M3 15V4.5" />
-        </svg>
-        <p className='text-xs'>Report</p>
+        <div className='pl-1 flex items-center'>
+        <button className='flex items-center flex-col hover:underline' onClick={handleReportClick}>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M3 3v1.5M3 21v-6m0 0l2.77-.693a9 9 0 016.208.682l.108.054a9 9 0 006.086.71l3.114-.732a48.524 48.524 0 01-.005-10.499l-3.11.732a9 9 0 01-6.085-.711l-.108-.054a9 9 0 00-6.208-.682L3 4.5M3 15V4.5" />
+          </svg>
+          <p className='text-xs'>Report</p>
         </button>
-        <button className='flex items-center flex-col hover:underline' onClick={()=>{setStarred(!isStarred)}}>
-            <svg xmlns="http://www.w3.org/2000/svg" fill={isStarred?"rgb(246,190,0)":"none"} viewBox="0 0 24 24" strokeWidth={1.5} stroke="rgb(246,190,0)" className="w-6 h-6">
-            <path strokeLinecap="round" strokeLinejoin="round" d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
-            </svg>
-            <p className='text-xs'>Star post</p>
-        </button>
+  
+        {/* Report Box */}
+        {showReportBox && (
+          <div className="fixed  bottom-0 left-0 w-full h-full bg-gray-800 bg-opacity-50 flex justify-center items-center zIndex = 200">
+            <div className="bg-white p-4 rounded-md">
+              <p>Report Reason:</p>
+              <textarea
+                value={reportReason}
+                onChange={(e) => setReportReason(e.target.value)}
+                className="w-full p-2 border border-gray-300 rounded-md"
+                placeholder="Enter your report reason..."
+              />
+              <button
+                onClick={handleReportSubmit}
+                className="mt-2 px-4 py-2 bg-blue-700 text-white rounded-md"
+              >
+                Submit Report
+              </button>
+            </div>
+          </div>
+        )}
         <button className='flex items-center flex-col hover:underline' onClick={()=>{setStarred(!isStarred)}}>
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
             <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 12.76c0 1.6 1.123 2.994 2.707 3.227 1.087.16 2.185.283 3.293.369V21l4.076-4.076a1.526 1.526 0 011.037-.443 48.282 48.282 0 005.68-.494c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0012 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018z" />
