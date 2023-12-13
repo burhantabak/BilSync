81cef0f291d4c335b7b5cbd9a99c968e9ed4e793
Kenan <kanan.zeynalov@ug.bilkent.edu.tr>
2023-12-13 19:08:03 +0300

    addAccount changed

--- Frontend/SynchFront/src/statics/AddAccount.jsx
+++ Frontend/SynchFront/src/statics/AddAccount.jsx
@@ -8,7 +8,7 @@ export default function AddAccount() {
   const [profileImage, setProfileImage] = useState(null);
   const [email, setEmail] = useState('');
 
-  const navigate = useNavigate(); 
+  const navigate = useNavigate();
 
   const handleAccountTypeChange = (event) => {
     setAccountType(event.target.value);
@@ -32,85 +32,103 @@ export default function AddAccount() {
 
   const handleSubmit = (event) => {
     event.preventDefault();
+    // Add your submission logic here
   };
 
-  const handleGoBack = () => { // This function is used to navigate back to the previous page
-    navigate(-1); // Navigate back to the previous page
-};
-
+  const handleGoBack = () => {
+    navigate(-1);
+  };
 
   return (
-    <div className="flex flex-col h-full bg-gray-100">
-        <div className="flex items-center mb-4">
-                     <button
-                        className="text-blue-500 hover:underline"
-                        onClick={handleGoBack}
-                     >
-                        &larr; Back
-                    </button>
-                </div>
-        
-      <div className="flex flex-row justify-between px-4 py-2 bg-gray-200">
-        <h1 className="text-xl font-bold">Add Account</h1>
-      </div>
-      <form className="flex flex-col overflow-auto px-4 py-2" onSubmit={handleSubmit}>
-        <div className="mb-4">
-          <label className="block text-sm font-medium text-gray-700">Account Type</label>
-          <select
-            name="accountType"
-            value={accountType}
-            onChange={handleAccountTypeChange}
-            className="rounded-md border border-gray-300 px-2 py-1"
-          >
-            <option value="">Choose an account type</option>
-            <option value="student">Student</option>
-            <option value="alumni">Alumni</option>
-            <option value="instructor">Instructor</option>
-        </select>
-        </div>
-        <div className="mb-4">
-          <label className="block text-sm font-medium text-gray-700">Name</label>
-          <input
-            type="text"
-            name="name"
-            value={name}
-            onChange={handleNameChange}
-            className="rounded-md border border-gray-300 px-2 py-1"
-          />
-        </div>
-        <div className="mb-4">
-          <label className="block text-sm font-medium text-gray-700">Surname</label>
-          <input
-            type="text"
-            name="surname"
-            value={surname}
-            onChange={handleSurnameChange}
-            className="rounded-md border border-gray-300 px-2 py-1"
-          />
-        </div>
-        <div className="mb-4">
-          <label className="block text-sm font-medium text-gray-700">Upload Profile Image</label>
-          <input
-            type="file"
-            name="profileImage"
-            onChange={handleProfileImageChange}
-            className="rounded-md border border-gray-300 px-2 py-1"
-          />
-        </div>
-        <div className="mb-4">
-          <label className="block text-sm font-medium text-gray-700">Add Email</label>
-          <input
-            type="email"
-            name="email"
-            value={email}
-            onChange={handleEmailChange}
-            className="rounded-md border border-gray-300 px-2 py-1"
-          />
-        </div>
-        <button type="submit" className="rounded-md bg-blue-500 text-white px-4 py-1 hover:bg-blue-600">
-          Add Account
+    <div className="bg-blue-500 min-h-screen flex flex-col items-center justify-center">
+      <div className="mb-4">
+        <button
+          className="text-white"
+          onClick={handleGoBack}
+        >
+          &larr; Back
         </button>
-      </form>
-    </div>
-)};
+      </div>
+      <div className="bg-white rounded p-8 shadow-md w-full max-w-md">
+        <h1 className="text-blue-500 text-2xl font-bold mb-6">Add Account</h1>
 
+        <form onSubmit={handleSubmit}>
+          <div className="mb-4">
+            <label className="block text-gray-700 mb-2" htmlFor="accountType">
+              Account Type
+            </label>
+            <select
+              name="accountType"
+              value={accountType}
+              onChange={handleAccountTypeChange}
+              className="rounded-md border border-gray-300 px-4 py-2 w-full focus:outline-none focus:border-blue-500 transition duration-300 ease-in-out"
+            >
+              <option value="">Choose an account type</option>
+              <option value="student">Student</option>
+              <option value="alumni">Alumni</option>
+              <option value="instructor">Instructor</option>
+            </select>
+          </div>
+
+          <div className="mb-4">
+            <label className="block text-gray-700 mb-2" htmlFor="name">
+              Name
+            </label>
+            <input
+              type="text"
+              name="name"
+              value={name}
+              onChange={handleNameChange}
+              className="rounded-md border border-gray-300 px-4 py-2 w-full focus:outline-none focus:border-blue-500 transition duration-300 ease-in-out"
+            />
+          </div>
+
+          <div className="mb-4">
+            <label className="block text-gray-700 mb-2" htmlFor="surname">
+              Surname
+            </label>
+            <input
+              type="text"
+              name="surname"
+              value={surname}
+              onChange={handleSurnameChange}
+              className="rounded-md border border-gray-300 px-4 py-2 w-full focus:outline-none focus:border-blue-500 transition duration-300 ease-in-out"
+            />
+          </div>
+
+          <div className="mb-4">
+            <label className="block text-gray-700 mb-2" htmlFor="profileImage">
+              Upload Profile Image
+            </label>
+            <input
+              type="file"
+              name="profileImage"
+              onChange={handleProfileImageChange}
+              className="rounded-md border border-gray-300 px-4 py-2 w-full focus:outline-none focus:border-blue-500 transition duration-300 ease-in-out"
+            />
+          </div>
+
+          <div className="mb-4">
+            <label className="block text-gray-700 mb-2" htmlFor="email">
+              Add Email
+            </label>
+            <input
+              type="email"
+              name="email"
+              value={email}
+              onChange={handleEmailChange}
+              className="rounded-md border border-gray-300 px-4 py-2 w-full focus:outline-none focus:border-blue-500 transition duration-300 ease-in-out"
+            />
+          </div>
+
+          <button
+            type="submit"
+            className="bg-blue-500 text-white font-bold px-4 py-2 rounded w-full md:w-auto"
+          >
+            Add Account
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+}
