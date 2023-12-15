b0a983167b782e41dbc905a69412ccc7dd71530a
Tuna <tuna.saygin@ug.bilkent.edu.tr>
2023-12-15 21:07:22 +0300

    kardeşim kaşlar güzel olmuş gerçekten

--- Frontend/SynchFront/src/Components/HelperFunctions/DateFormat.jsx
+++ Frontend/SynchFront/src/Components/HelperFunctions/DateFormat.jsx
@@ -1,14 +1,12 @@
 export default function formatDate(dateString) {
-    const options = { day: 'numeric', month: 'short', year: 'numeric' };
+    const options = { day: 'numeric', month: 'short', year: 'numeric', timeZone: 'Europe/Istanbul' };
     const date = new Date(dateString);
     return `${date.toLocaleDateString('en-US', options)}`;
 }
 export function formatTime(dateString) {
-    const date = new Date(dateString);
-    const hours = formatTwoDigits(date.getHours());
-    const minutes = formatTwoDigits(date.getMinutes());
-  
-    return `${hours}:${minutes}`;
+  const options = { hour: 'numeric', minute: 'numeric', hour12: false, timeZone: 'Europe/Istanbul' };
+  const date = new Date(dateString);
+  return date.toLocaleTimeString('en-US', options);
   }
   
   function formatTwoDigits(value) {
