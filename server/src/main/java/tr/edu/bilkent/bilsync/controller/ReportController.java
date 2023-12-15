e9c9efdfc456d3f67d50ec8867796e2843312fa6
Işıl <isilozgu2016@gmail.com>
2023-12-15 13:30:48 +0300

    no report condition handled

--- server/src/main/java/tr/edu/bilkent/bilsync/controller/ReportController.java
+++ server/src/main/java/tr/edu/bilkent/bilsync/controller/ReportController.java
@@ -7,12 +7,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.*;
 import tr.edu.bilkent.bilsync.entity.Report;
+import tr.edu.bilkent.bilsync.exception.NoRecordFoundException;
 import tr.edu.bilkent.bilsync.exception.UserIsBannedException;
 import tr.edu.bilkent.bilsync.service.ReportService;
-import tr.edu.bilkent.bilsync.service.UserInfoService;
 
-import java.util.List;
-
+/**
+ * REST controller for handling admin-related operations on reports.
+ */
 @RestController
 @RequestMapping("/admin/reports")
 public class ReportController {
@@ -20,18 +21,39 @@ public class ReportController {
     private final ReportService reportService;
 
 
+    /**
+     * Constructs a new ReportController with the specified service dependency.
+     *
+     * @param reportService The service responsible for report-related operations.
+     */
     @Autowired
     public ReportController(ReportService reportService) {
         this.reportService = reportService;
 
     }
 
+    /**
+     * Retrieves all reports.
+     *
+     * @return A ResponseEntity containing an iterable of reports.
+     */
     @GetMapping("/all")
-    public ResponseEntity<Iterable<Report>> getAllReports() {
-        Iterable<Report> reports = reportService.getAllReports();
-        return ResponseEntity.ok(reports);
+    public ResponseEntity<?> getAllReports() {
+        try {
+            Iterable<Report> reports = reportService.getAllReports();
+            return ResponseEntity.ok(reports);
+        } catch (NoRecordFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("No records found in the database for YourEntity");
+        }
     }
 
+    /**
+     * Deletes a report with the specified ID.
+     *
+     * @param reportId The ID of the report to be deleted.
+     * @return A ResponseEntity with a status code and a message.
+     */
     @DeleteMapping("/delete/{reportId}")
     public ResponseEntity<String> deleteReport(@PathVariable Long reportId) {
         try {
@@ -42,18 +64,36 @@ public class ReportController {
         }
     }
 
+    /**
+     * Views details of a reported post.
+     *
+     * @param reportId The ID of the report associated with the post.
+     * @return A ResponseEntity with a status code and a message.
+     */
     @GetMapping("/{reportId}/viewPost")
     public ResponseEntity<String> viewReportedPost(@PathVariable Long reportId) {
         // Implement logic to view details of a report
         return new ResponseEntity<>("Report details", HttpStatus.OK);
     }
 
+    /**
+     * Views details of a reported comment.
+     *
+     * @param reportId The ID of the report associated with the comment.
+     * @return A ResponseEntity with a status code and a message.
+     */
     @GetMapping("/{reportId}/viewComment")
     public ResponseEntity<String> viewReportedComment(@PathVariable Long reportId) {
         // Implement logic to view details of a report
         return new ResponseEntity<>("Report details", HttpStatus.OK);
     }
 
+    /**
+     * Bans the user reported in a specific report.
+     *
+     * @param reportId The ID of the report associated with the user to be banned.
+     * @return A ResponseEntity with a status code and a message.
+     */
     @PostMapping("/{reportId}/ban")
     public ResponseEntity<?> banReportedUser(@PathVariable Long reportId) {
 
