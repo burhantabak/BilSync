9c0d540b83931fbb19a9b66f17c3fed36525d6be
burhantabak <burhantabak@gmail.com>
2023-12-16 15:04:00 +0300

    image fix trial

--- server/src/main/java/tr/edu/bilkent/bilsync/entity/Image.java
+++ server/src/main/java/tr/edu/bilkent/bilsync/entity/Image.java
@@ -2,15 +2,25 @@ package tr.edu.bilkent.bilsync.entity;
 
 import jakarta.persistence.*;
 
+import java.io.File;
+import java.nio.file.Paths;
+
 @Entity
 @Table(name = "image")
 public class Image {
 
-    private static final String IMAGE_BASE_PATH = "./chatImages";
+    private static final String IMAGE_BASE_PATH = "." + File.separator + "chatImages";
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    public Image() {
+    }
+
+    public Image(Long id){
+        this.id = id;
+    }
 
     public void setId(Long id) {
         this.id = id;
@@ -20,7 +30,7 @@ public class Image {
         return id;
     }
 
-    public String getPath(){
-        return IMAGE_BASE_PATH + "image_" + id + ".dat";
+    public String getPath() {
+        return Paths.get(IMAGE_BASE_PATH, "image_" + id + ".dat").toString();
     }
 }
